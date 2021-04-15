dbaa850139b47ef79a4afb8bef213d2e00b2f8cd
beatles0 <lsmod99@gmail.com>
2021-04-15 22:10:37 +0900

    04-15

---
+++ app/src/main/java/com/example/ybkim/resouce.kt
@@ -0,0 +1,31 @@
+package com.example.ybkim
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_resouce.*
+
+class resouce : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resouce)
+
+        // 1
+        var ment = resources.getString(R.string.hello)
+        Log.d("ment", ment)
+
+        // 2
+        val ment2 = getString(R.string.hello)
+        Log.d("ment", ment)
+
+        // 3
+        val color2 = resources.getColor(R.color.black)
+        Log.d("ment", "color:" + color2)
+
+        //4
+        val color3 = getColor(R.color.black)
+        Log.d("ment", "color:" + color3)
+
+        mybutton.setBackgroundColor( getColor(R.color.design_default_color_error) )
+    }
+}
