e786c58c1944e9ea36a729f5af9153d0e3d75f22
beatles0 <lsmod99@gmail.com>
2021-04-16 14:19:30 +0900

    사무실 테스트

---
+++ app/src/main/java/com/example/ybkim/AddViewList.kt
@@ -0,0 +1,11 @@
+package com.example.ybkim
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddViewList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_view_list)
+    }
+}
