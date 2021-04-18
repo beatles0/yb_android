636cdebc10e7b0d7160f1a4e4b4c08721eed1cf5
beatles0 <lsmod99@gmail.com>
2021-04-18 19:03:03 +0900

    modify file name

--- app/src/main/java/com/example/ybkim/IntentFirst.kt
+++ app/src/main/java/com/example/ybkim/IntentFirst.kt
@@ -1,23 +1,59 @@
 package com.example.ybkim
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_intent_first.*
 
-class IntentFirst : AppCompatActivity() {
+abstract class IntentFirst : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intent_first)
 
+        Log.d("intent:", "intentFirst Create")
+        Log.d("intent:", intent.getStringExtra("Data1").toString())
+        Log.d("intent:", intent.getStringExtra("Data2").toString())
+
+
+        /* btn click */
         btn_first_move.setOnClickListener {
-            Log.d("intent:", "============================")
+            Log.d("intent:", "============first================")
             Log.d("intent: ", "btn first move click ")
 
-            var  itnt_first = Intent (this@IntentFirst , IntentSecond::class.java)
-            startActivity(itnt_first)
+            val YB_REQUEST_CODE: Int = 100
+            var itnt_first = Intent(this@IntentFirst, IntentSecond::class.java)
+                // 변수 넘기기
+                .apply {
+                    putExtra("Data1", "first->second 오마이 갓 김치")
+                    putExtra("Data2", "first->second 오마이 가스라이터")
+                }
+
+            // startActivity 만 호출하면 second 가 finish 될때 변수를 받지 못한다.
+            // startActivity(itnt_first)
+
+            // second 가  finish 될때 변수를 받으려면 아래처럼 startActivityForResult  를 호출 해야 한다.
+            startActivityForResult(itnt_first, YB_REQUEST_CODE)
+
 
         }
+
     }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Log.d("intent: ", "first onActivityResult call ")
+        Log.d("intent: ", "requestCode:  $requestCode ")
+        Log.d("intent: ", "Activity.RESULT_OK:  ${Activity.RESULT_OK} ")
+        if ( resultCode == Activity.RESULT_OK) {
+            Log.d("intent: ", "result ok ")
+            Log.d("intent:", data?.getStringExtra("Data1").toString())
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/ybkim/IntentSecond.kt
+++ app/src/main/java/com/example/ybkim/IntentSecond.kt
@@ -1,5 +1,7 @@
 package com.example.ybkim
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -12,13 +14,40 @@ class IntentSecond : AppCompatActivity() {
 
         Log.d("intent:", "intentSecond Create")
 
+        Log.d("intent:", intent.getStringExtra("Data1").toString())
+        Log.d("intent:", intent.getStringExtra("Data2").toString())
+
+        /* btn click */
         btn_second_move.setOnClickListener {
+            Log.d("intent:", "============second================")
             Log.d("intent: ", "btn second  move click")
+            /*
+            아래처럼 finish  로 해도 second activity 가 종료되므로 첫번째 화면이 나온다.
+             */
+            //            finish()
+
+            /* 이렇게 아래처럼 하면 변수가 잘 가긴 한다 */
+            var itnt_second = Intent(this@IntentSecond, IntentFirst::class.java)
+                // 변수 넘기기
+                .apply {
+                    putExtra("Data1", "second->first 오마이 갓김치")
+                    putExtra("Data2", "second->first 오마이 가스라이터")
+                }
+            startActivity(itnt_second)
+        }
+
+        // 종료 버튼을 누르면 실행되는 콜백 함수
+        btn_second_finish.setOnClickListener {
+            Log.d("intent: ", "btn second  finish click")
+
+            var returnIntent = Intent()
+            returnIntent.putExtra("Data1", "종료되면서 return  하는 데이터")
+            setResult(Activity.RESULT_OK, returnIntent)
             finish()
         }
 
-    }
 
+    }
 
 
 }
