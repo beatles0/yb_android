089d0dad70ec983893b902483106594d9514bb04
beatles0 <lsmod99@gmail.com>
2021-04-20 16:04:28 +0900

    04-20

--- app/src/main/java/com/example/ybkim/TelephoneDetail.kt
+++ app/src/main/java/com/example/ybkim/TelephoneDetail.kt
@@ -2,10 +2,25 @@ package com.example.ybkim
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.OnBackPressedDispatcherOwner
+import kotlinx.android.synthetic.main.activity_telephone_detail.*
 
 class TelephoneDetail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_telephone_detail)
+
+        displayDetail()
+        id_go_back.setOnClickListener {
+            onBackPressed()
+        }
     }
+
+    fun displayDetail() {
+        id_nick_name.setText(intent.getStringExtra("nickName"))
+        id_real_name.setText(intent.getStringExtra("realName"))
+        id_phone_number.setText(intent.getStringExtra("phoneNum"))
+    }
+
 }
--- app/src/main/java/com/example/ybkim/TelephoneList.kt
+++ app/src/main/java/com/example/ybkim/TelephoneList.kt
@@ -1,13 +1,18 @@
 package com.example.ybkim
 
+//import com.example.ybkim.databinding.ActivityBindingTestBinding
+//import com.example.ybkim.databinding.ActivityTelephoneListBinding
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.example.ybkim.databinding.ActivityBindingTestBinding
-import com.example.ybkim.databinding.ActivityTelephoneListBinding
+import kotlinx.android.synthetic.main.activity_binding_test.*
+import kotlinx.android.synthetic.main.activity_telephone_list.*
 import kotlinx.android.synthetic.main.activity_telephone_list_in.*
 
 class TelephoneList : AppCompatActivity() {
@@ -15,8 +20,13 @@ class TelephoneList : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_telephone_list)
 
-        var arrPhoneList = ArrayList<PhoneListCls>()
+        var arrPhoneList: ArrayList<PhoneListCls> = addTelephoneList()
+        displayTelephoneList(arrPhoneList)
 
+    } // end of create
+
+    fun addTelephoneList(): ArrayList<PhoneListCls> {
+        var arrPhoneList = ArrayList<PhoneListCls>()
         var strRandomNick: String = ""
         var strRandomPhone: String = ""
         var strRandomName: String = ""
@@ -25,64 +35,49 @@ class TelephoneList : AppCompatActivity() {
             strRandomNick = "별명-" + (0..100).random().toString()
             strRandomName = "이름-" + (0..100).random().toString()
             strRandomPhone = "010-" + (0..100).random().toString() + "-" + (0..100).random().toString() + "-" + (0..100).random().toString()
-
-            arrPhoneList.add ( PhoneListCls( nickName= strRandomNick, realName= strRandomName, phoneNum= strRandomPhone) )
+            arrPhoneList.add(PhoneListCls(nickName = strRandomNick, realName = strRandomName, phoneNum = strRandomPhone))
         }
+        return arrPhoneList
+    }
 
-        //  출력해보기
-        /*
-        for ( i in 0 until ( arrPhoneList.size -1) ) {
-            Log.d("phone: ", "========= $i =================" )
-            Log.d("phone: ", arrPhoneList.get(i).nickName )
-            Log.d("phone: ", arrPhoneList.get(i).realName )
-            Log.d("phone: ", arrPhoneList.get(i).phoneNum )
-            Log.d("phone: ", "==========================" )
-
-        }
-        */
-
-        // view binding 을 사용한 예제.
-        /*
-        binding = ActivityBindingTestBinding.inflate(layoutInflater)
-        var root_view = binding.root
-        setContentView(root_view)
-
-        binding.idButton.setOnClickListener {
-            if (toggle) binding.idTextView.text = "ok click"
-            else binding.idTextView.text = "Hello World"
-            toggle = !toggle
-        }
-        */
-
-        // inflate 하기
+    fun displayTelephoneList(arrPhoneList: ArrayList<PhoneListCls>) {
         val container = findViewById<LinearLayout>(R.id.id_phone_list)
-
         val inflater: LayoutInflater = LayoutInflater.from(this@TelephoneList)
 
         for (i in 0 until arrPhoneList.size - 1) {
             val itemView = inflater.inflate(R.layout.activity_telephone_list_in, null)
             var nickNameView = itemView.findViewById<TextView>(R.id.id_nick_name)
-            val phoneNumView= itemView.findViewById<TextView>(R.id.id_phone_number)
+            val phoneNumView = itemView.findViewById<TextView>(R.id.id_phone_number)
 
             nickNameView.setText(arrPhoneList.get(i).nickName)
             phoneNumView.setText(arrPhoneList.get(i).phoneNum)
+
+            clickListerPhone(arrPhoneList.get(i) , itemView )
+
             container.addView(itemView)
-            Log.d("car: ", arrPhoneList.get(i).nickName + " / " + arrPhoneList.get(i).phoneNum)
         }
-
-        id_nick_name.setOnClickListener {
-            
-        }
-
     }
 
-     /*
-     아래처럼 picture: String = "" default 생성자를 주니까 위에
-     arrPhoneList.add ( PhoneListCls( nickName= strRandomNick, realName= strRandomName, phoneNum= strRandomName) )
-     에서 애러가 나지 않는다.
-     */
+    fun clickListerPhone(arrPhoneList: PhoneListCls, view: View) {
+        view.setOnClickListener {
+            val YB_REQUEST_CODE: Int = 100
+            var itnt_first = Intent(this@TelephoneList , TelephoneDetail::class.java)
+                    // 변수 넘기기
+                    .apply {
+                        putExtra("nickName", arrPhoneList.nickName)
+                        putExtra("realName", arrPhoneList.realName)
+                        putExtra("phoneNum", arrPhoneList.phoneNum)
+                    }
+            startActivityForResult(itnt_first, YB_REQUEST_CODE)
+        }
+    }
 
-    class PhoneListCls(var nickName: String, var realName: String, var phoneNum: String, var picture: String= "")
-    { }
+/*
+아래처럼 picture: String = "" default 생성자를 주니까 위에
+arrPhoneList.add ( PhoneListCls( nickName= strRandomNick, realName= strRandomName, phoneNum= strRandomName) )
+에서 애러가 나지 않는다.
+*/
+
+    class PhoneListCls(var nickName: String, var realName: String, var phoneNum: String, var picture: String = "") {}
 }
 
